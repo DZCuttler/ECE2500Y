3b962d2e4ddf6592c07bf2933a8b0a6ba042cbf8
David Cuttler <18dzc@queensu.ca>
2025-08-05 15:01:18 -0400

    jooob

--- job.sh
+++ job.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-romang      # Replace with your PI's allocation/account
 #SBATCH --job-name=my_project_job
-#SBATCH --output=logs/%x-%j.out         # Save stdout to logs/jobname-jobid.out
-#SBATCH --error=logs/%x-%j.err
 #SBATCH --time=01:00:00                 # Max runtime (HH:MM:SS)
-#SBATCH --mem=4G                        # Memory per node
-#SBATCH --cpus-per-task=2              # Number of CPU cores
+#SBATCH --mem=8G                        # Memory per node
+#SBATCH --cpus-per-task=4              # Number of CPU cores
 #SBATCH --mail-user=david.cuttler@mail.utoronto.ca       # (Optional) get email updates
 #SBATCH --mail-type=ALL           # (Optional) email on end/fail
 
@@ -26,7 +24,7 @@ module load python/3.10 scipy-stack
 # virtualenv --no-download $SLURM_TMPDIR/env
 # source $SLURM_TMPDIR/env/bin/activate
 # pip install --no-index --upgrade pip
-source ~/myEnv/bin/activate
+# source ~/myEnv/bin/activate
 
 # pip install --no-index -r ~/requirements.txt
 
